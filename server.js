73fa9ad69005c7ffede32ce19ebd65cd0ccf4579
mitchell.wright877@gmail.com <mitchell.wright877@gmail.com>
2020-05-11 14:10:13 +0100

    Reorganised

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-
 const qutil = require('./qutil.js')
 const multer = require('multer');
+const GoogleAuth = require('simple-google-openid');
+
+const app = express();
 const uploader = multer({ dest: 'surveys' })
 
 app.use(express.static('client', { extensions: ['html'] }))
 
-app.get('/questionnairess', asyncWrap(async (req, res) => {
-    const content = await qutil.content();
-    console.log();
-    res.send(JSON.stringify(content));;
-}));
+/**
+    Will serve to register all uploaded JSON questionnaires
+    under the user that's uploaded them
+*/
+async function registerQuestionnaire(req, res) {
 
-app.post('/questionnaires', uploader.single('questionnaire'), express.json(), asyncWrap(async (req, res) => {
-    console.log('gottem');
-    res.send("Done");
-}));
+    const fname = req.file.filename;
+    console.log(`File received: ${fname}`);
+    res.send('Done!');
 
+}
+
+// Async wrapper
 function asyncWrap(f) {
     return (req, res, next) => {
     Promise.resolve(f(req, res, next))
@@ -27,10 +30,14 @@ function asyncWrap(f) {
   };
 }
 
+app.post('/questionnaires', uploader.single('questionnaire'),
+    express.json(), asyncWrap(registerQuestionnaire));
+
 // Start server
 app.listen(8080, async () => {
 
+    // Initialize questionnaires module
     await qutil.init();
-
     console.log('Listening on port 8080!');
-})
+
+});
